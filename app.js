4723f1f976be55a8e92464a4fc2de7c74beabf38
Louis <lj13561@my.bristol.ac.uk>
2016-06-13 16:21:05 +0100

    Just about to change collection layout to include pots in a seperate collection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var uuid = require('uuid');
 
 // For parsing responses from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
 var db = require('monk')('localhost:27017/Lettus-Grow');
+// Collections
 var users = db.get('users');
+var plants = db.get('plants');
+var pots = db.get('pots');
 
 // users.insert({ userId: 'Louis' });
 // users.insert({ name: 'Jon', plants: {} });
@@ -27,9 +31,34 @@ users.findAndModify({ query: { userId: 'Louis' }, update: {
           blue: 0.5,
           red: 0.4
         },
-        startTime: '20:30', // Format change? 20*60 + 30 = 1230
+        startTime: '20:30',
         endTime: '10:30'
-      }
+      },
+      // Should be limited to four
+      plants: [
+        null,
+        null,
+        {
+          UUID: '59ffe42f-2184-4a3d-8e21-5789d385b0f5',
+          type: 'Basil',
+          planted: '05-01-2016',
+          // position: 3,
+          history: [
+            {
+              timeStamp: 1465550685, // http://www.unixtimestamp.com/
+              light: {
+                intensities: {
+                  blue: 0.5,
+                  red: 0.4
+                },
+                startTime: '20:30',
+                endTime: '10:30'
+              }
+            }
+          ]
+        },
+        null
+      ]
     },
     {
       type: 'herbGarden',
@@ -41,7 +70,31 @@ users.findAndModify({ query: { userId: 'Louis' }, update: {
         },
         startTime: '21:30', // Format change? 20*60 + 30 = 1230
         endTime: '11:30'
-      }
+      },
+      plants: [
+        null,
+        {
+          UUID: 'f67549e7-e251-4fd7-a71c-f9e6b3cd9916',
+          type: 'Basil',
+          // position: 2,
+          planted: '05-01-2016',
+          history: [
+            {
+              timeStamp: 1465550685, // http://www.unixtimestamp.com/
+              light: {
+                intensities: {
+                  blue: 0.5,
+                  red: 0.4
+                },
+                startTime: '20:30',
+                endTime: '10:30'
+              }
+            }
+          ]
+        },
+        null,
+        null
+      ]
     }
   ]
 }});
@@ -57,25 +110,84 @@ users.findAndModify({ query: { userId: 'Louis' }, update: {
 //   console.log(docs);
 // });
 
-// Formally Jade
-app.set('view engine', 'pug');
+app.set('view engine', 'pug'); // Formally Jade
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
+
+app.post('/addPlant', function(req, res) {
+  var updatedInfo = req.body;
+
+  // Get current state of pot
+  users.find({ "pots.UUID": updatedInfo.potUUID }).on("success", function(doc) {
+    console.log();
+    console.log(doc.pots[parseInt(updatedInfo.potNumber)]);
+    console.log();
+
+    var setModifier = { $set: {} };
+
+    setModifier.$set["pots.$.plants." + updatedInfo.position] = {
+      UUID: uuid.v4(), // Create a fresh UUID
+      type: updatedInfo.type,
+      planted: updatedInfo.plantedTime,
+      history: [ // Create the first history insert
+        {
+          timeStamp: Math.floor(Date.now() / 1000), // Time now in seconds
+          light: {
+            intensities: {
+              blue: 0.5,
+              red: 0.4
+            },
+            startTime: '20:30',
+            endTime: '10:30'
+          }
+        }
+      ]
+    }
+
+    users.update(
+      { "pots.UUID": updatedInfo.potUUID },
+      setModifier
+    ).on("success", function(doc) { 
+      res.redirect('/');
+    });
+  });
+});
+
+
+
+app.post('/removePlant', function(req, res) {
+  var updatedInfo = req.body,
+      setModifier = { $set: {} };
+
+  setModifier.$set["pots.$.plants." + updatedInfo.position] = null;
+
+  users.update(
+    { "pots.UUID": updatedInfo.potUUID },
+    setModifier
+  ).on("success", function(doc) { 
+    res.redirect('/');
+  });
+});
+
+
+
+
 
 // Authentication required
 app.post('/update', function(req, res) {
   var updatedInfo = req.body;
 
   users.update(
-    { "pots.UUID": req.body.UUID },
-    { $set: {
-      "pots.$.light.intensities.blue": updatedInfo.blueIntensity,
-      "pots.$.light.intensities.red": updatedInfo.redIntensity,
-      "pots.$.light.startTime": updatedInfo.startTime,
-      "pots.$.light.endTime": updatedInfo.endTime
-    }}
-  ).on('success', function(doc) { console.log("Successfully updated DB"); });
+    { "pots.UUID": updatedInfo.UUID },
+    { $set:
+      {
+        "pots.$.light.intensities.blue": updatedInfo.blueIntensity,
+        "pots.$.light.intensities.red": updatedInfo.redIntensity,
+        "pots.$.light.startTime": updatedInfo.startTime,
+        "pots.$.light.endTime": updatedInfo.endTime
+      }
+  }).on('success', function(doc) { console.log("Successfully updated DB"); });
 
   res.redirect('/');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,29 @@
 window.onload = function() {
-    $('.startTime').combodate({firstItem: 'none'});
-    $('.endTime').combodate({firstItem: 'none'}); 
+  $('.plantedTime').combodate({
+    firstItem: 'none',
+    minYear: 2016,
+    maxYear: 2016,
+    smartDays: true,
+  });
+  $('.startTime').combodate({firstItem: 'none'});
+  $('.endTime').combodate({firstItem: 'none'}); 
 };
+
+function newPlant(checked, thing1, thing2) {
+  console.log(checked);
+  // console.log(i);
+  console.log(thing1);
+  console.log(thing2);
+
+  if(checked) {
+    // thing1.disabled = "";
+    console.log(thing1.parentNode.parentNode.childNodes.forEach());
+  } else {
+    // thing1.disabled = "disabled";
+    // thing2.disabled = "disabled";
+  }
+
+  // $('.plantedTime');
+
+  // plantSubmit + i + ".hidden = (plantInput" + i + ".checked) ? '' : 'hidden'
+}
