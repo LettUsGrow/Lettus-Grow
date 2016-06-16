eddc79e6cd835c3b1f1d6b8ad9001cdb2215fc7f
Louis <lj13561@my.bristol.ac.uk>
2016-06-16 08:54:32 +0100

    Prior to AWS

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express();
 var uuid = require('uuid');
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+var moment = require('moment');
+var bcrypt = require('bcrypt');
+var ensure = require('connect-ensure-login');
+const saltRounds = 10;
 
 var https = require('http');
-// var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-// var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
 var httpServer = https.createServer(app);
 
@@ -15,32 +17,25 @@ var db = require('monk')('localhost:27017/Lettus-Grow');
 // Collections
 var users = db.get('users');
 var plantsAlive = db.get('plantsAlive');
+plantsAlive.options.multi = true;
 var plantsDead = db.get('plantsDead');
 var pots = db.get('pots');
 
 // Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
   function(username, password, done) {
     users.findOne({username: username}, function(err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (user.password != password) { return done(null, false); }
-      return done(null, user);
+
+      bcrypt.compare(password, user.password, function(err, res) {
+        if (res == false) { return done(null, false); }
+        return done(null, user);
+      });
     });
   }));
 
 // Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
 passport.serializeUser(function(user, done) {
   done(null, user._id);
 });
@@ -52,11 +47,9 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(require('cookie-parser')());
+app.use(require('cookie-parser')('keyboard cat'));
 app.use(require('body-parser').urlencoded({ extended: true })); // For parsing responses from a POST
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // cookie: { maxAge: 10 * 60 * 1000 }
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -69,14 +62,16 @@ var testUUID_0 = uuid.v4(),
     testUUID_1 = uuid.v4(),
     testUUID_2 = uuid.v4();
 
-users.insert({
-  username: 'louis',
-  password: 'test',
-  pots: [testUUID_0, testUUID_1] 
+bcrypt.hash('password', saltRounds, function(err, hash) {
+  users.insert({
+    username: 'louis',
+    password: hash,
+    pots: [testUUID_0, testUUID_1] 
+  });
 });
 
 pots.insert({
-  type: 'herb',
+  type: 'Herb',
   UUID: testUUID_0,
   light: {
     intensities: {
@@ -86,11 +81,10 @@ pots.insert({
     startTime: '20:30',
     endTime: '10:30'
   },
-  // plants: [null, null, null, null]
 });
 
 pots.insert({
-  type: 'herb',
+  type: 'Herb',
   UUID: testUUID_1,
   light: {
     intensities: {
@@ -100,19 +94,20 @@ pots.insert({
     startTime: '12:30',
     endTime: '08:30'
   },
-  // plants: [null, null, null, null]
 });
 
 plantsAlive.insert({
   type: 'Lettuce',
-  planted: '11-05-2016',
+  planted: '14-06-2016',
+  health: 'medium',
   position: 1,
   potUUID: testUUID_0
 });
 
 plantsAlive.insert({
   type: 'Basil',
-  planted: '14-05-2016',
+  planted: '15-06-2016',
+  health: 'good',
   position: 3,
   potUUID: testUUID_1
 });
@@ -122,81 +117,110 @@ app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
 app.post('/addPlant', 
-  require('connect-ensure-login').ensureLoggedIn(),
+  ensure.ensureLoggedIn(),
   function(req, res) {
-  var update = req.body;
+    var update = req.body;
 
-  // Get current state of pot for history purposes
-  pots.findOne({ 'UUID': update.potUUID }, function(err, pot) {
-    
-    // Check if there's space?
-    plantsAlive.insert({ 
-      type: update.type,
-      position: parseInt(update.position),
-      potUUID: update.potUUID,
-      planted: update.planted,
+    // Get current state of pot for history purposes
+    pots.findOne({ 'UUID': update.potUUID }, function(err, pot) {      
+      // Check if there's space?
+      plantsAlive.insert({ 
+        type: update.type,
+        position: parseInt(update.position),
+        potUUID: update.potUUID,
+        planted: update.planted,
+        health: 'good',
 
-      // Create the first history insert
-      history: [{ 
-        timeStamp: Math.floor(Date.now() / 1000),
-        light: pot.light
-      }]
+        // Create the first history insert
+        history: [{ 
+          timeStamp: Math.floor(Date.now() / 1000),
+          light: pot.light
+        }]
 
-    }, function(err, plant) {
+      }, function(err, plant) {
 
-      res.redirect('/');
+        res.redirect('/');
+      });
     });
   });
-});
 
 
 app.post('/removePlant', 
-  require('connect-ensure-login').ensureLoggedIn(),
+  ensure.ensureLoggedIn(),
   function(req, res) {
-  var update = req.body;
+    var update = req.body;
 
-  // Get current state of pot for history purposes
-  pots.findOne({ 'UUID': update.potUUID }, function(err, pot) {
+    // Get current state of pot for history purposes
+    pots.findOne({ 'UUID': update.potUUID }, function(err, pot) {
 
-    plantsAlive.findOne({ "potUUID": update.potUUID, "position": parseInt(update.position) }, function(err, plant) {
+      plantsAlive.findOne({ "potUUID": update.potUUID, "position": parseInt(update.position) }, function(err, plant) {
 
-      if(plant.history)
-        plant.history.push({
-          timeStamp: Math.floor(Date.now() / 1000),
-          light: pot.light
-        });
+        if(plant.history)
+          plant.history.push({
+            timeStamp: Math.floor(Date.now() / 1000),
+            light: pot.light
+          });
 
-      plantsDead.insert(plant, function(err) {
+        plantsDead.insert(plant, function(err) {
 
-        plantsAlive.remove({ "potUUID": update.potUUID, "position": parseInt(update.position) }, function(err) {
-          
-          res.redirect('/');
+          plantsAlive.remove({ "potUUID": update.potUUID, "position": parseInt(update.position) }, function(err) {
+            
+            res.redirect('/');
+          });
         });
       });
     });
   });
-});
 
+app.post('/updateHealth',
+  ensure.ensureLoggedIn(), function(req, res) {
+
+    var update = req.body,
+        updateHistory = {
+          timeStamp: Math.floor(Date.now() / 1000),
+          health: update.health
+        };
+
+    plantsAlive.update({ potUUID: update.potUUID, position: parseInt(update.position) }, 
+      { $push: { history: updateHistory }}, function(err) {
+
+      if(err) throw err;
+
+      plantsAlive.update({ potUUID: update.potUUID, position: parseInt(update.position) }, 
+        { $set: { health: update.health }}, function(err) {
+        
+        if(err) throw err;
+
+        res.redirect('/');
+      });
+    });
+  });
 
 app.post('/updatePot', 
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res) {
-  var update = req.body,
-      light = {
-        intensities: {
-          blue: +update.blueIntensity,
-          red: +update.redIntensity
-        },
-        startTime: update.startTime,
-        endTime: update.endTime
-      };
+  ensure.ensureLoggedIn(), function(req, res) {
 
-  pots.update({ "UUID": update.UUID },
-    { $set: { "light": light }
-  }).on('success', function(doc) { 
-    res.redirect('/');
+    var update = req.body,
+        updateLight = {
+          intensities: {
+            blue: +update.blueIntensity,
+            red: +update.redIntensity
+          },
+          startTime: update.startTime,
+          endTime: update.endTime
+        },
+        updateHistory = {
+          timeStamp: Math.floor(Date.now() / 1000),
+          light: updateLight
+        };
+
+    pots.update({ "UUID": update.UUID }, { $set: { "light": updateLight }}, function(doc) {
+
+      plantsAlive.update({ "potUUID": update.UUID }, { $push: { history: updateHistory }}, function(err) {
+
+        res.redirect('/');
+      });
+    });
   });
-});
 
 app.get('/login',
   function(req, res){
@@ -221,7 +245,7 @@ app.get('/logout',
   });
 
 app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
+  ensure.ensureLoggedIn(),
   function (req, res) {
 
     users.findOne({ _id: req.user._id }, function (err, user) { // Get the user
@@ -234,7 +258,14 @@ app.get('/profile',
             return a.position - b.position;
           });
 
+          // Get age parameter
+          usersPlants = usersPlants.map(function(obj) {
+            obj.planted = age(obj.planted);
+            return obj;
+          });
+
           res.render('index', {
+            todaysDate: moment().format('DD-MM-YYYY'),
             user: user,
             pots: usersPots ? usersPots : [],
             plants: usersPlants ? usersPlants : []
@@ -242,6 +273,15 @@ app.get('/profile',
         });
       });
     });
+  });
+
+function age(date) {
+ return moment(new Date(date.split('-').reverse().join('-'))).fromNow(); 
+}
+
+app.use(function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
   });
 
 httpServer.listen(8000);
--- public/js/main.js
+++ public/js/main.js
@@ -8,22 +8,3 @@ window.onload = function() {
   $('.startTime').combodate({firstItem: 'none'});
   $('.endTime').combodate({firstItem: 'none'}); 
 };
-
-// function newPlant(checked, thing1, thing2) {
-//   console.log(checked);
-//   // console.log(i);
-//   console.log(thing1);
-//   console.log(thing2);
-
-//   if(checked) {
-//     // thing1.disabled = "";
-//     console.log(thing1.parentNode.parentNode.childNodes.forEach());
-//   } else {
-//     // thing1.disabled = "disabled";
-//     // thing2.disabled = "disabled";
-//   }
-
-//   // $('.plantedTime');
-
-//   // plantSubmit + i + ".hidden = (plantInput" + i + ".checked) ? '' : 'hidden'
-// }
